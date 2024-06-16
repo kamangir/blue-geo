5cc3435c9440dfdf4217964422756186c9cedfa6
kamangir <arash@kamangir.net>
2024-06-15 22:48:49 -0700

    lint refactor - kamangir/bolt#746

--- blue_geo/.abcli/blue_geo.sh
+++ blue_geo/.abcli/blue_geo.sh
@@ -8,6 +8,12 @@ function blue_geo() {
         return
     fi
 
+    if [ "$task" == "pylint" ]; then
+        abcli_${task} ignore=blue_geo/QGIS,plugin=blue_geo,$2 \
+            "${@:3}"
+        return
+    fi
+
     abcli_generic_task \
         plugin=blue_geo,task=$task \
         "${@:2}"

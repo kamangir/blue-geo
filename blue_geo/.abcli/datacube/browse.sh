a04a813555fbdeab26e55d2028113c4b97c05b0e
kamangir <arash@kamangir.net>
2024-07-20 13:14:56 -0700

    untested code ❗️ - kamangir/bolt#746

--- blue_geo/.abcli/datacube/browse.sh
+++ blue_geo/.abcli/datacube/browse.sh
@@ -4,7 +4,9 @@ function blue_geo_datacube_browse() {
     local options=$1
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
-        :
+        options="catalog=<catalog>"
+        abcli_show_usage "@datacube browse$ABCUL[$options]$ABCUL<args>" \
+            "list catalogs."
         return
     fi
 

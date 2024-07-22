e9122fe933465953e7c28c63fd6608ebabb47e6d
kamangir <arash@kamangir.net>
2024-07-21 18:35:11 -0700

    test refactors - kamangir/bolt#746

--- blue_geo/tests/test_catalog.py
+++ blue_geo/tests/test_catalog.py
@@ -7,21 +7,19 @@ from blue_geo.datacube.catalogs import catalog_of
     ["datacube_id", "expected_success", "expected_catalog"],
     [
         [
-            [
-                asset
-                + [
-                    "generic",
-                ]
-                for asset in assets.datacube_generic_parse_datacube_id
-            ]
+            asset
             + [
-                asset
-                + [
-                    "firms_area",
-                ]
-                for asset in assets.datacube_firms_area_parse_datacube_id
+                "generic",
             ]
-        ],
+            for asset in assets.datacube_generic_parse_datacube_id
+        ]
+        + [
+            asset
+            + [
+                "firms_area",
+            ]
+            for asset in assets.datacube_firms_area_parse_datacube_id
+        ]
     ],
 )
 def test_catalog_of(

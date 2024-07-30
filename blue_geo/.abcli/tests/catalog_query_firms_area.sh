acd44a5224712533ed89853fa52cbc5a21eda217
kamangir <arash@kamangir.net>
2024-07-29 18:58:39 -0700

    bashtest fix, hopefully - kamangir/bolt#746

--- blue_geo/.abcli/tests/catalog_query_firms_area.sh
+++ blue_geo/.abcli/tests/catalog_query_firms_area.sh
@@ -5,12 +5,15 @@ function test_blue_geo_catalog_query_firms_area() {
 
     local object_name=firms-$(abcli_string_timestamp_short)
 
-    blue_geo datacube query firms_area \
-        ingest $object_name - \
+    blue_geo catalog query firms \
+        ingest \
+        $object_name \
+        area \
         --date 2024-07-20
+    [[ $? -ne 0 ]] && return 1
 
     abcli_assert \
-        $(blue_geo datacube query read len $object_name) \
+        $(blue_geo catalog query read len $object_name) \
         1
     [[ $? -ne 0 ]] && return 1
 

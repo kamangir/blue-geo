b3262db958dc12ee83598093cd4517a520eff8eb
kamangir <arash@kamangir.net>
2024-07-30 21:16:17 -0700

    rebuild - kamangir/bolt#746

--- blue_geo/.abcli/actions.sh
+++ blue_geo/.abcli/actions.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
 function blue_geo_action_git_before_push() {
-    if [[ "$(abcli_git get_branch)" == "main" ]]; then
-        blue_geo build_README
+    blue_geo build_README
+
+    [[ "$(abcli_git get_branch)" == "main" ]] &&
         blue_geo pypi build
-    fi
 }

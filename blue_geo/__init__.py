b97438827d695ac6298c55127417a51098ded973
kamangir <arash@kamangir.net>
2024-08-17 19:32:27 -0700

    copernicus refactor - kamangir/bolt#746

--- blue_geo/__init__.py
+++ blue_geo/__init__.py
@@ -4,7 +4,7 @@ ICON = "🌐"
 
 DESCRIPTION = f"{ICON} AI for precise geospatial data analysis and visualization."
 
-VERSION = "4.224.1"
+VERSION = "4.225.1"
 
 REPO_NAME = "blue-geo"
 
--- blue_geo/catalog/copernicus/classes.py
+++ blue_geo/catalog/copernicus/classes.py
@@ -1,5 +1,14 @@
+from typing import List
+from pystac_client import Client  # https://pystac-client.readthedocs.io/en/stable/
 from blue_geo.catalog.generic import GenericCatalog
 
 
 class CopernicusCatalog(GenericCatalog):
     name = "copernicus"
+
+    # https://documentation.dataspace.copernicus.eu/APIs/STAC.html
+    url = "https://catalogue.dataspace.copernicus.eu/stac"
+
+    def get_collection_names(self) -> List[str]:
+        client = Client.open(self.url)
+        return [collection.id for collection in client.get_collections()]

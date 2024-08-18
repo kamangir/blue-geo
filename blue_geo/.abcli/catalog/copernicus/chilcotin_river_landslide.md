9504cf2badc7e4dcedae37adc81d04b4b9c588d4
kamangir <arash@kamangir.net>
2024-08-18 11:12:58 -0700

    copernicus query - kamangir/bolt#746

--- blue_geo/.abcli/catalog/copernicus/chilcotin_river_landslide.md
+++ blue_geo/.abcli/catalog/copernicus/chilcotin_river_landslide.md
@@ -1,4 +1,4 @@
-# chilcotin river landslide
+# Chilcotin River Landslide
 
 ![image](https://github.com/kamangir/assets/blob/main/blue-geo/chilcotin-river-landslide-2.jpg?raw=true)
 
--- blue_geo/__init__.py
+++ blue_geo/__init__.py
@@ -4,7 +4,7 @@ ICON = "🌐"
 
 DESCRIPTION = f"{ICON} AI for precise geospatial data analysis and visualization."
 
-VERSION = "4.233.1"
+VERSION = "4.234.1"
 
 REPO_NAME = "blue-geo"
 

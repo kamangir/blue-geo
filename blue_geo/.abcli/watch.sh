064d38188b8d0b360b3af008ae2226568dd8b51f
kamangir <arash@kamangir.net>
2024-08-24 11:40:28 -0700

    xtra and xwrap - kamangir/bolt#746

--- blue_geo/.abcli/watch.sh
+++ blue_geo/.abcli/watch.sh
@@ -2,22 +2,28 @@
 
 function blue_geo_watch() {
     local options=$1
-    local workflow_options=$2
+    local target_options=$2
+    local workflow_options=$3
 
     if [ $(abcli_option_int "$options" help 0) == 1 ]; then
         local list_of_targets=$(python3 -m blue_geo.watch list \
-            --log 0 \
             --what targets \
-            --delim \|)
-        options="dryrun,upload"
-        target_options="target=$list_of_targets$EOP,datetime=<2024-07-30/2024-08-15>,lat=<51.83>,lon=<-122.78>$EOPE"
-        workflow_options="${EOP}dryrun,${EOPE}to=$NBS_RUNNERS_LIST"
-        abcli_show_usage "@geo watch$ABCUL[$options]$ABCUL[$target_options]$ABCUL[$workflow_options]$ABCUL[-|<object-name>]" \
+            --delim \| \
+            --log 0)
+
+        options="$(xtra dryrun,)frames=<frames>$(xtra ,upload)"
+
+        target_options="$(xtra 'dryrun,datetime=<2024-07-30/2024-08-15>,lat=<51.83>,lon=<-122.78>,')target=$list_of_targets"
+
+        workflow_options="$(xtra dryrun,)to=$NBS_RUNNERS_LIST"
+
+        abcli_show_usage "@geo watch $(xwrap $options $target_options $workflow_options '-|<object-name>')" \
             "watch a target."
+
         return
     fi
 
-    local object_name=$(abcli_clarify_object $3 watching-$target-$(abcli_string_timestamp))
+    local object_name=$(abcli_clarify_object $4 geo-watch-$(abcli_string_timestamp))
 
     echo "🪄"
 }

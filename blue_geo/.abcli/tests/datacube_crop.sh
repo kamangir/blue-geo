dda165c62756800947c52900b0f278814b573ad0
kamangir <arash@kamangir.net>
2024-08-26 20:23:45 -0700

    bash test for @datacube crop - kamangir/bolt#746

---
+++ blue_geo/.abcli/tests/datacube_crop.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+function test_blue_geo_datacube_crop() {
+    local options=$1
+
+    local datacube_id=$BLUE_GEO_TEST_DATACUBE_COPERNICUS_SENTINEL_2_CHILCOTIN
+    blue_geo_datacube_ingest \
+        what=quick,$options \
+        $datacube_id
+
+    local suffix=test_blue_geo_datacube_crop-$(abcli_string_timestamp_short)
+
+    blue_geo_datacube_crop \
+        suffix=$suffix,$options \
+        $BLUE_GEO_TEST_DATACUBE_CROP_CTULINE \
+        $datacube_id
+}

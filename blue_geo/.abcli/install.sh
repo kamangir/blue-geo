43026b00b2d5157c3af298ee99875d1e650508b5
kamangir <arash@kamangir.net>
2024-08-26 20:02:49 -0700

    gdal install refactor - kamangir/bolt#746

--- blue_geo/.abcli/install.sh
+++ blue_geo/.abcli/install.sh
@@ -3,7 +3,8 @@
 function abcli_install_blue_geo() {
     if [[ "$abcli_is_mac" == true ]]; then
         brew install gdal
-    elif [[ "$abcli_is_ubuntu" == true ]]; then
+    elif [[ "$abcli_is_ubuntu" == true ]] &&
+        [[ "$abcli_is_github_workflow" == false ]]; then
         apt-get install -y gdal-bin
     fi
 }

94538af30bd394b692d375b77fc3e5efed07e805
kamangir <arash@kamangir.net>
2024-09-02 21:57:32 -0700

    next: ingest_file - kamangir/bolt#746

--- blue_geo/catalog/EarthSearch/sentinel_2_l1c/classes.py
+++ blue_geo/catalog/EarthSearch/sentinel_2_l1c/classes.py
@@ -16,9 +16,24 @@ class EarthSearchSentinel2L1CDatacube(STACDatacube):
 
     name = "sentinel_2_l1c"
 
+    s3_prefix = "s3://sentinel-s2-l1c/tiles"
+
     def list_of_files(
         self,
         scope: DatacubeScope = DatacubeScope("all"),
         verbose: bool = False,
     ) -> List[str]:
-        return ["fake"]
+        list_of_items = [asset.href for asset in self.metadata["Item"].assets.values()]
+
+        list_of_files: List[str] = []
+        quick_found = False
+        for item in list_of_items:
+            item_filename = item.split(f"{self.s3_prefix}/", 1)[1].replace("/", "_")
+
+            includes, quick_found = scope.includes(
+                item_filename, -1, verbose, quick_found
+            )
+            if includes:
+                list_of_files.append(item_filename)
+
+        return list_of_files

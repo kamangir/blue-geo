9c1b25611bc1b04b31d4820237e850a74e910ec6
kamangir <arash@kamangir.net>
2024-09-03 18:32:39 -0700

    doc refactor - kamangir/bolt#746

--- blue_geo/README.py
+++ blue_geo/README.py
@@ -6,35 +6,23 @@ from blue_geo import NAME, VERSION, ICON, REPO_NAME
 
 
 features = {
-    "QGIS": {
-        "description": "an AI terraform for [QGIS](https://www.qgis.org/).",
-        "icon": ICON,
-        "thumbnail": "https://raw.githubusercontent.com/kamangir/assets/main/blue-geo/QGIS.jpg",
-        "url": "https://github.com/kamangir/blue-geo/blob/main/blue_geo/QGIS/README.md",
-    },
-    "datacube": {
-        "description": "a datacube for geospatial AI.",
-        "icon": "🧊",
-        "thumbnail": "https://github.com/kamangir/assets/blob/main/blue-geo/datacube.png?raw=true",
-        "url": "https://github.com/kamangir/blue-geo/tree/main/blue_geo/datacube",
-    },
     "copernicus": {
         "description": "catalog: [Copernicus Data Space Ecosystem - Europe's eyes on Earth](https://dataspace.copernicus.eu/)",
         "icon": "🧊",
         "thumbnail": "https://github.com/kamangir/assets/blob/main/blue-geo/copernicus.jpg?raw=true",
         "url": "https://github.com/kamangir/blue-geo/tree/main/blue_geo/catalog/copernicus",
     },
-    "geo-watch": {
-        "description": "watching targets through `@geo`.",
-        "icon": ICON,
-        "thumbnail": "https://github.com/kamangir/assets/blob/main/blue-geo/blue-geo-watch.png?raw=true",
-        "url": "https://github.com/kamangir/blue-geo/blob/main/blue_geo/watch",
-    },
     "firms-area": {
         "description": "catalog: Fire Information for Resource Management System ([FIRMS](https://firms.modaps.eosdis.nasa.gov)).",
         "icon": ICON,
         "thumbnail": "https://raw.githubusercontent.com/kamangir/assets/main/blue-geo/datacube-firms_area.jpg",
         "url": "https://github.com/kamangir/blue-geo/tree/main/blue_geo/catalog/firms",
+    },
+    "EarthSearch": {
+        "description": "catalog: [Earth Search by Element 84 (earth-search-aws)](https://stacindex.org/catalogs/earth-search#/).",
+        "icon": ICON,
+        "thumbnail": "https://github.com/kamangir/assets/blob/main/blue-geo/viewer-aws-element84-com.png?raw=true",
+        "url": "https://github.com/kamangir/blue-geo/blob/main/blue_geo/catalog/EarthSearch",
     },
     "ukraine-timemap": {
         "description": "catalog: [Bellingcat](https://www.bellingcat.com/) [Civilian Harm in Ukraine TimeMap](https://github.com/bellingcat/ukraine-timemap) dataset, available through [this UI](https://ukraine.bellingcat.com/) and [this API](https://bellingcat-embeds.ams3.cdn.digitaloceanspaces.com/production/ukr/timemap/api.json).",
@@ -48,11 +36,23 @@ features = {
         "thumbnail": "https://kamangir-public.s3.ca-central-1.amazonaws.com/test_vancouver_watching_ingest/animation.gif?raw=true",
         "url": "https://github.com/kamangir/Vancouver-Watching",
     },
-    "EarthSearch": {
-        "description": "catalog: [Earth Search by Element 84 (earth-search-aws)](https://stacindex.org/catalogs/earth-search#/).",
+    "geo-watch": {
+        "description": "watching targets through `@geo`.",
         "icon": ICON,
-        "thumbnail": "https://github.com/kamangir/assets/blob/main/blue-geo/viewer-aws-element84-com.png?raw=true",
-        "url": "https://github.com/kamangir/blue-geo/blob/main/blue_geo/catalog/EarthSearch",
+        "thumbnail": "https://github.com/kamangir/assets/blob/main/blue-geo/blue-geo-watch.png?raw=true",
+        "url": "https://github.com/kamangir/blue-geo/blob/main/blue_geo/watch",
+    },
+    "QGIS": {
+        "description": "an AI terraform for [QGIS](https://www.qgis.org/).",
+        "icon": ICON,
+        "thumbnail": "https://raw.githubusercontent.com/kamangir/assets/main/blue-geo/QGIS.jpg",
+        "url": "https://github.com/kamangir/blue-geo/blob/main/blue_geo/QGIS/README.md",
+    },
+    "datacube": {
+        "description": "a datacube for geospatial AI.",
+        "icon": "🧊",
+        "thumbnail": "https://github.com/kamangir/assets/blob/main/blue-geo/datacube.png?raw=true",
+        "url": "https://github.com/kamangir/blue-geo/tree/main/blue_geo/datacube",
     },
     "template": {
         "description": "",

77a80b9ff070fa79488ecc4516b3116c5fbe9b60
kamangir <arash@kamangir.net>
2024-09-07 19:52:42 -0700

    doc refactor - kamangir/bolt#746

--- blue_geo/__init__.py
+++ blue_geo/__init__.py
@@ -1,11 +1,13 @@
+from blue_objects.env import ABCLI_PUBLIC_PREFIX
+
 NAME = "blue_geo"
 
 ICON = "🌐"
 
 DESCRIPTION = f"{ICON} AI for a Blue Planet."
 
-VERSION = "4.450.1"
+VERSION = "4.451.1"
 
 REPO_NAME = "blue-geo"
 
-MARQUEE = "https://kamangir-public.s3.ca-central-1.amazonaws.com/2024-01-06-20-39-46-73614/2024-01-06-20-39-46-73614-2X.gif?raw=true"
+MARQUEE = f"{ABCLI_PUBLIC_PREFIX}/2024-01-06-20-39-46-73614/2024-01-06-20-39-46-73614-2X.gif?raw=true"

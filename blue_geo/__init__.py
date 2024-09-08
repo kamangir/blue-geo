2d336dce7b89a7a08516038be94b80ff1eb8bd27
kamangir <arash@kamangir.net>
2024-09-07 19:14:44 -0700

    doc refactor: - kamangir/bolt#746

--- blue_geo/__init__.py
+++ blue_geo/__init__.py
@@ -4,7 +4,7 @@ ICON = "🌐"
 
 DESCRIPTION = f"{ICON} AI for precise geospatial data analysis and visualization."
 
-VERSION = "4.446.1"
+VERSION = "4.447.1"
 
 REPO_NAME = "blue-geo"
 
--- blue_geo/watch/targets/jasper.py
+++ blue_geo/watch/targets/jasper.py
@@ -1,5 +1,4 @@
-from functools import reduce
-from typing import List
+from typing import List, Dict
 from blue_objects.env import ABCLI_PUBLIC_PREFIX
 
 list_of_dates: List[str] = [
@@ -8,14 +7,16 @@ list_of_dates: List[str] = [
     "2024_7_27",
 ]
 
-items = reduce(
-    lambda x, y: x + y,
-    [
-        [
-            "`{}`".format(date.replace("_", "-")),
-            f"![image]({ABCLI_PUBLIC_PREFIX}/geo-watch-2024-09-06-Jasper-a/11_U_MU_{date}_0_TCI.png)",
-        ]
-        for date in list_of_dates
-    ],
-    [],
-)
+date_items: Dict[str, List[str]] = {
+    date: [
+        "`{}`".format(date.replace("_", "-")),
+        f"![image]({ABCLI_PUBLIC_PREFIX}/geo-watch-2024-09-06-Jasper-a/11_U_MU_{date}_0_TCI.png)",
+    ]
+    for date in list_of_dates
+}
+
+items: List[str] = [
+    date_items[date][index]
+    for index in range(len(date_items[list_of_dates[0]]))
+    for date in list_of_dates
+]

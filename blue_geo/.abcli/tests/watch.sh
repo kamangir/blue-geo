5a9e57b9b45b1a9fce67ea6752ea30bfa14ccd33
kamangir <arash@kamangir.net>
2024-09-29 22:06:31 -0700

    test refactor - kamangir/bolt#746

--- blue_geo/.abcli/tests/watch.sh
+++ blue_geo/.abcli/tests/watch.sh
@@ -3,11 +3,26 @@
 function test_blue_geo_watch() {
     local options=$1
 
+    local object_name=test_blue_geo_watch-$(abcli_string_timestamp)
+
     blue_geo_watch \
         ,$options \
         target=test \
         to=local \
         - \
-        publish \
-        test_blue_geo_watch
+        - \
+        $object_name
+
+    abcli_clone \
+        ~download \
+        $object_name \
+        test_blue_geo_watch_v2
+
+    abcli_publish \
+        tar \
+        test_blue_geo_watch_v2
+
+    abcli_publish \
+        suffix=.gif \
+        test_blue_geo_watch_v2
 }

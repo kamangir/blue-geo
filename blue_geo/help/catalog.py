317e357580af2af928793440e5d9c35428927c6a
kamangir <arash@kamangir.net>
2024-09-29 17:36:23 -0700

    doc refactor - kamangir/bolt#746

--- blue_geo/help/catalog.py
+++ blue_geo/help/catalog.py
@@ -25,4 +25,17 @@ def get(
             mono=mono,
         )
 
+    if tokens[0] == "get":
+        what = "is_STAC|url:<...>|url_args|list_of_args"
+        args = ["[--catalog <catalog>]"]
+        return show_usage(
+            [
+                "@catalog get",
+                f"[{what}]",
+            ]
+            + args,
+            "get catalog properties.",
+            mono=mono,
+        )
+
     return ""

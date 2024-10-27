724377a9457e200862e35c5d3304d725428ec9b7
kamangir <arash@kamangir.net>
2024-10-27 15:15:39 -0700

    watch diff refactor - kamangir/bolt#746

--- blue_geo/watch/algo/diff/map.py
+++ blue_geo/watch/algo/diff/map.py
@@ -64,10 +64,11 @@ def map_function(
             objects.path_of(
                 filename="metadata.yaml",
                 object_name=index_object_name,
-            )
+            ),
+            ignore_error=True,
         )
         if not success:
-            return success
+            break
 
         if index in [0, 1]:
             filename = objects.path_of(
@@ -75,124 +76,126 @@ def map_function(
                 object_name=index_object_name,
             )
             if not file.exists(filename):
-                logger.error(f"{filename} not found!")
-                return False
+                logger.warning(f"{filename} not found!")
+                success = False
+                break
 
             if index == 0:
                 baseline_filename = filename
             else:
                 target_filename = filename
 
-    success, baseline_image, _ = GenericDatacube.load_modality(
-        filename=baseline_filename,
-        modality="rgb",
-        log=True,
-    )
+    if success:
+        success, baseline_image, _ = GenericDatacube.load_modality(
+            filename=baseline_filename,
+            modality="rgb",
+            log=True,
+        )
+
+    if success:
+        success, target_image, _ = GenericDatacube.load_modality(
+            filename=target_filename,
+            modality="rgb",
+            log=True,
+        )
+
+    if success:
+        diff_image = np.squeeze(target_image[:, :, 0] - baseline_image[:, :, 0]).astype(
+            np.float32
+        )
+        diff_image[diff_image < -diff_range] = -diff_range
+        diff_image[diff_image > diff_range] = diff_range
+
+        plt.figure(figsize=(10, 6))
+        plt.hist(
+            diff_image.ravel(),
+            bins=256,
+            range=(-diff_range, diff_range),
+        )
+        plt.title(
+            justify_text(
+                " | ".join(
+                    [
+                        "diff histogram",
+                        query_object_name,
+                        f"/{suffix}",
+                        f"@{offset}+{depth}",
+                        f"+-{diff_range:.2f}",
+                        file.name_and_extension(baseline_filename),
+                        file.name_and_extension(target_filename),
+                    ]
+                ),
+                line_width=line_width,
+                return_str=True,
+            )
+        )
+        plt.xlabel(
+            justify_text(
+                " | ".join(["DN diff"] + signature()),
+                line_width=line_width,
+                return_str=True,
+            )
+        )
+        plt.ylabel("frequency")
+        plt.grid(True)
+        success = file.save_fig(
+            objects.path_of("diff-histogram.png", object_name),
+            log=True,
+        )
+
+    if success:
+        colored_diff = cv2.applyColorMap(
+            ((diff_image / diff_range + 1) / 2 * 255).astype(np.uint8), cv2.COLORMAP_JET
+        )
+
+        gradient = (
+            255
+            * np.linspace(0, 1, colored_diff.shape[0]).reshape(-1, 1)
+            * np.ones((1, colorbar_width))
+        ).astype(np.uint8)
+        colorbar = cv2.applyColorMap(gradient, cv2.COLORMAP_JET)
+        concatenated_image = np.hstack(
+            (
+                colored_diff,
+                np.zeros(
+                    (colored_diff.shape[0], colorbar_width // 2, 3),
+                    dtype=np.uint8,
+                ),
+                colorbar,
+            )
+        )
+        colored_diff_signed = add_signature(
+            concatenated_image,
+            header=[
+                " | ".join(
+                    objects.signature(
+                        " | ".join(
+                            [
+                                suffix,
+                                f"@{offset}+{depth}",
+                                f"+-{diff_range:.2f}",
+                                file.name_and_extension(baseline_filename),
+                                file.name_and_extension(target_filename),
+                            ]
+                        ),
+                        query_object_name,
+                    )
+                ),
+            ],
+            footer=[
+                target.one_liner,
+                " | ".join(signature()),
+            ],
+            word_wrap=True,
+        )
+        success = file.save_image(
+            objects.path_of("diff.png", object_name),
+            colored_diff_signed,
+            log=True,
+        )
+
     if not success:
-        return success
-
-    success, target_image, _ = GenericDatacube.load_modality(
-        filename=target_filename,
-        modality="rgb",
-        log=True,
-    )
-    if not success:
-        return success
-
-    diff_image = np.squeeze(target_image[:, :, 0] - baseline_image[:, :, 0]).astype(
-        np.float32
-    )
-    diff_image[diff_image < -diff_range] = -diff_range
-    diff_image[diff_image > diff_range] = diff_range
-
-    plt.figure(figsize=(10, 6))
-    plt.hist(
-        diff_image.ravel(),
-        bins=256,
-        range=(-diff_range, diff_range),
-    )
-    plt.title(
-        justify_text(
-            " | ".join(
-                [
-                    "diff histogram",
-                    query_object_name,
-                    f"/{suffix}",
-                    f"@{offset}+{depth}",
-                    f"+-{diff_range:.2f}",
-                    file.name_and_extension(baseline_filename),
-                    file.name_and_extension(target_filename),
-                ]
-            ),
-            line_width=line_width,
-            return_str=True,
-        )
-    )
-    plt.xlabel(
-        justify_text(
-            " | ".join(["DN diff"] + signature()),
-            line_width=line_width,
-            return_str=True,
-        )
-    )
-    plt.ylabel("frequency")
-    plt.grid(True)
-    if not file.save_fig(
-        objects.path_of("diff-histogram.png", object_name),
-        log=True,
-    ):
-        return False
-
-    colored_diff = cv2.applyColorMap(
-        ((diff_image / diff_range + 1) / 2 * 255).astype(np.uint8), cv2.COLORMAP_JET
-    )
-
-    gradient = (
-        255
-        * np.linspace(0, 1, colored_diff.shape[0]).reshape(-1, 1)
-        * np.ones((1, colorbar_width))
-    ).astype(np.uint8)
-    colorbar = cv2.applyColorMap(gradient, cv2.COLORMAP_JET)
-    concatenated_image = np.hstack(
-        (
-            colored_diff,
-            np.zeros(
-                (colored_diff.shape[0], colorbar_width // 2, 3),
-                dtype=np.uint8,
-            ),
-            colorbar,
-        )
-    )
-    colored_diff_signed = add_signature(
-        concatenated_image,
-        header=[
-            " | ".join(
-                objects.signature(
-                    " | ".join(
-                        [
-                            suffix,
-                            f"@{offset}+{depth}",
-                            f"+-{diff_range:.2f}",
-                            file.name_and_extension(baseline_filename),
-                            file.name_and_extension(target_filename),
-                        ]
-                    ),
-                    query_object_name,
-                )
-            ),
-        ],
-        footer=[
-            target.one_liner,
-            " | ".join(signature()),
-        ],
-        word_wrap=True,
-    )
-    if not file.save_image(
-        objects.path_of("diff.png", object_name),
-        colored_diff_signed,
-        log=True,
-    ):
-        return False
+        logger.warning("not usable.")
 
     return post_to_object(
         object_name,

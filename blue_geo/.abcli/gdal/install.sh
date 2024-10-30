0208543312cc99ae1c2c3f8ee0adcecc8ba39073
kamangir <arash@kamangir.net>
2024-10-30 11:47:16 -0700

    gdal refactor - kamangir/bolt#746

--- blue_geo/.abcli/gdal/install.sh
+++ blue_geo/.abcli/gdal/install.sh
@@ -9,7 +9,7 @@ function blue_geo_gdal_install() {
     if [[ "$abcli_is_ubuntu" == true ]] &&
         [[ "$abcli_is_github_workflow" == false ]] &&
         [[ "$abcli_is_docker" == false ]]; then
-        sudo add-apt-repository ppa:ubuntugis/ppa
+        # sudo add-apt-repository ppa:ubuntugis/ppa
         sudo apt-get update
         sudo apt-get install -y gdal-bin python3-gdal
     fi
--- blue_geo/__init__.py
+++ blue_geo/__init__.py
@@ -9,7 +9,7 @@ ICON = "🌐"
 
 DESCRIPTION = f"{ICON} AI for a Blue Planet."
 
-VERSION = "4.710.1"
+VERSION = "4.711.1"
 
 REPO_NAME = "blue-geo"
 

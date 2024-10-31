1416640fc71c3c5a9d40aaed4f8071d8d5c82837
kamangir <arash@kamangir.net>
2024-10-30 17:59:58 -0700

    nbs refactor - kamangir/bolt#746

--- blue_geo/.abcli/watch.sh
+++ blue_geo/.abcli/watch.sh
@@ -123,7 +123,7 @@ function blue_geo_watch() {
     [[ "$do_submit" == 0 ]] && return 0
 
     abcli_eval dryrun=$do_dryrun \
-        workflow submit \
+        notebooks_and_scripts_workflow_submit \
         ~download,$workflow_options \
         $job_name
 }
--- blue_geo/__init__.py
+++ blue_geo/__init__.py
@@ -9,7 +9,7 @@ ICON = "🌐"
 
 DESCRIPTION = f"{ICON} AI for a Blue Planet."
 
-VERSION = "4.712.1"
+VERSION = "4.713.1"
 
 REPO_NAME = "blue-geo"
 

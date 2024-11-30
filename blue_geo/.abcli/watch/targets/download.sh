ecb8184b22e5f42c28a3372fd17f1d59d1b0495d
kamangir <arash@kamangir.net>
2024-11-30 12:36:15 -0800

    @targets download refactor - kamangir/bolt#746

--- blue_geo/.abcli/watch/targets/download.sh
+++ blue_geo/.abcli/watch/targets/download.sh
@@ -7,5 +7,9 @@ function blue_geo_watch_targets_download() {
         $BLUE_GEO_WATCH_TARGET_LIST \
         "$@"
 
-    python3 -m blue_geo.watch.targets list
+    abcli_list_log $(python3 -m blue_geo.watch.targets \
+        list \
+        --log 0) \
+        --before "downloaded" \
+        --after "target(s)"
 }
